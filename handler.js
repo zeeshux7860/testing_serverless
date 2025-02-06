e8d82eea2ce039abd9ffb4a68db45006d70abfe5
zeeshux7860 <ctechviral@gmail.com>
2025-02-06 15:30:57 +0530

    ji

--- handler.js
+++ handler.js
@@ -2,8 +2,12 @@ const { sayHello } = require("./utils/helper");
 
 module.exports = async function (context, event) {
     let name = event.body ? event.body.toString() : "Guest";
-    return {
+    context.logger.infoWith('Adding to now', {
+        'request': request,
+        'to': now.format()
+    });
+    context.callback({
         statusCode: 200,
         body: sayHello(name)
-    };
+    })
 };
